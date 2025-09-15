d31e85037e6503b24d84776c732914803e716b8c
JesusOmar12 <21011055@itsoeh.edu.mx>
2025-09-15 11:07:07 -0600

    Modificacion 73

--- script.js
+++ script.js
@@ -36,12 +36,6 @@ function renderTableFirebase(snapshot) {
     keys.forEach(key => {
         const reg = data[key];
 
-
-// Formatear la fecha
-        const fecha = new Date(reg.Fecha_de_Mantenimiento);
-        const opciones = { day: '2-digit', month: '2-digit', year: 'numeric' };
-        const fechaFormateada = fecha.toLocaleDateString('es-ES', opciones);
-
         const tr = document.createElement('tr');
         tr.innerHTML = `
             <td>${reg.Nombre_del_Equipo}</td>
